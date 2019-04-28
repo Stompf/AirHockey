a5eb92a8b2a562fe8f1bcffe1a868f4a76eeac66
Stompf <anton.lundmark@gmail.com>
2019-04-28 22:12:08 +0200

    Tweaked network interval

--- src/server/config/index.ts
+++ src/server/config/index.ts
@@ -1,6 +1,6 @@
 export function getConfig() {
     return {
-        NETWORK_INTERVAL: Number(process.env.LUNNE_WEB_NETWORK_INTERVAL) || 1 / 20,
+        NETWORK_INTERVAL: Number(process.env.LUNNE_WEB_NETWORK_INTERVAL) || 1000 / 60,
         FIXED_TIME_STEP: Number(process.env.LUNNE_WEB_FIXED_TIME_STEP) || 1 / 60,
         MAX_SUB_STEPS: Number(process.env.LUNNE_WEB_MAX_SUB_STEPS) || 10,
     };
