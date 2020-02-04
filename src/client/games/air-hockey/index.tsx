a425c4fee551f07162b936321bdd7b531d2378d9
Stompf <anton.lundmark@gmail.com>
2020-02-04 18:01:30 +0100

    WIP

--- src/client/games/air-hockey/index.tsx
+++ src/client/games/air-hockey/index.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
-import { RouteComponentProps } from 'react-router-dom';
+import { Button, Grid, createStyles, WithStyles, withStyles } from '@material-ui/core';
 import { AirHockeyGame } from './scripts';
 
-export class AirHockey extends React.Component<RouteComponentProps<any>, {}> {
+const styles = createStyles({
+    button: {
+        width: 300,
+    },
+});
+
+type IAirHockeyProps = WithStyles<typeof styles>;
+
+export class AirHockeyComponent extends React.Component<IAirHockeyProps, {}> {
     private game: AirHockeyGame | undefined;
 
-    public render() {
-        return <div id="AirHockeyCanvas" />;
-    }
-
     public componentDidMount() {
-        this.game = new AirHockeyGame('AirHockeyCanvas');
+        // this.game = new AirHockeyGame('AirHockeyCanvas');
     }
 
     public componentWillUnmount() {
@@ -18,4 +22,30 @@ export class AirHockey extends React.Component<RouteComponentProps<any>, {}> {
             this.game.destroy(true);
         }
     }
+
+    public render() {
+        const { classes } = this.props;
+
+        return (
+            <Grid container direction="column" justify="center" alignItems="center" spacing={3}>
+                <Grid item xs={12}>
+                    <h1>Air Hockey</h1>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary" className={classes.button}>
+                        Local play
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" color="primary" className={classes.button}>
+                        Online play
+                    </Button>
+                </Grid>
+            </Grid>
+        );
+
+        //  return <div id="AirHockeyCanvas" />;
+    }
 }
+
+export const AirHockey = withStyles(styles)(AirHockeyComponent);
